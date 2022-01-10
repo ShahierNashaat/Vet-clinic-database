aee7a5337bb8cdf499b042ffe0c49bdf41b76e97
Shahier Nashaat <shahiern@yahoo.com>
2022-01-10 12:11:09 +0200

    Create animals table, insert data, execute queires

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+create database vet
+
+-- Create a table named animals
+create table animals (
+	id integer,
+	name varchar(200),
+	date_of_birth date,
+	escape_attempts integer,
+	neutered boolean,
+	weight_kg decimal
+)
