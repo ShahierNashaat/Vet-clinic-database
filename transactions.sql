4c36fee0e2bf78d4ef55ea53b7e055c2023f8d9c
Shahier Nashaat <shahiern@yahoo.com>
2022-01-11 16:37:28 +0200

    Implement the required changes

--- transactions.sql
+++ transactions.sql
@@ -3,7 +3,9 @@
 
 BEGIN;
 update animals set species = 'unspecified';
+select * from animals;
 ROLLBACK;
+select * from animals;
 
 
 -- Update the animals table by setting the species column to digimon for all animals 
@@ -13,16 +15,20 @@ ROLLBACK;
 
 BEGIN;
 update animals set species = 'digimon' where name like '%mon';
+select * from animals;
 update animals set species = 'pokemon' where species is NULL;
+select * from animals;
 COMMIT;
+select * from animals;
 
 
 -- delete all records in the animals table, then roll back the transaction.
 
 BEGIN;
 delete from animals;
+select * from animals;
 ROLLBACK;
-
+select * from animals;
 
 -- Delete all animals born after Jan 1st, 2022.
 -- Create a savepoint for the transaction.
@@ -32,8 +38,13 @@ ROLLBACK;
 
 BEGIN;
 delete from animals where date_of_birth > '2022-1-1';
+select * from animals;
 SAVEPOINT delete_by_date;
 update animals set weight_kg = weight_kg * -1;
+select * from animals;
 ROLLBACK TO delete_by_date;
+select * from animals;
 update animals set weight_kg = weight_kg * -1 where weight_kg < 0;
+select * from animals;
 COMMIT;
+select * from animals;
